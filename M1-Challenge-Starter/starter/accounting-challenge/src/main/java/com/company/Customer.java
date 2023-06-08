6765ad47b1dd44bdfe096b8e72cf88696b2a8036
liannnaa <lnp2016@nyu.edu>
2023-06-08 18:15:17 -0500

    added module 1 starter code

---
+++ M1-Challenge-Starter/starter/accounting-challenge/src/main/java/com/company/Customer.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+    private int id;
+    private String name;
+    private List<AccountRecord> charges = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        //update this
+        return 0;
+    }
+
+    public List<AccountRecord> getCharges() {
+        return charges;
+    }
+
+    @Override
+    public String toString() {
+        //update this
+        return "Update me";
+    }
+}
