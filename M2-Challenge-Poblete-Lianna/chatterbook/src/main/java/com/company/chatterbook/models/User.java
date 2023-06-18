3467a005931d00ab1c5ab996227ea5170b323b26
liannnaa <lnp2016@nyu.edu>
2023-06-18 17:25:01 -0500

    added User model

--- M2-Challenge-Poblete-Lianna/chatterbook/src/main/java/com/company/chatterbook/models/User.java
+++ M2-Challenge-Poblete-Lianna/chatterbook/src/main/java/com/company/chatterbook/models/User.java
@@ -1,6 +1,21 @@
 package com.company.chatterbook.models;
 
-public class User {
-    String name;
+import java.util.List;
 
+public class User {
+    private String name;
+    private List<ChatterPost> chatterPosts;
+
+    public User(String name, List<ChatterPost> chatterPosts) {
+        this.setName(name);
+        this.setChatterPosts(chatterPosts);
+    }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getName() { return name; }
+
+    public void setChatterPosts(List<ChatterPost> chatterPosts) { this.chatterPosts = chatterPosts; }
+
+    public List<ChatterPost> getChatterPosts() { return chatterPosts; }
 }
